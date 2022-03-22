ac2ab86c89c5908a4e55cb4712bfc32ef363a0c0
Chabrik Abdellatif <abdoo.chbrik@gmail.com>
2022-03-22 17:20:54 +0100

    boxs design

--- frontend_React/src/Component/main.jsx
+++ frontend_React/src/Component/main.jsx
@@ -9,6 +9,7 @@ import germany from '../assets/german.jpg'
 import Logo from '../assets/logo.png'
 export default function main() {
   return (
+     <div className='containerGlobal'>
     <div className='container'>
         <div className='container3'>
         <img src={Logo} className="image2"/>
@@ -35,10 +36,60 @@ export default function main() {
             <div className='container2'>
                 <img src={germany} className="image"/>
                 <p className='country'>allemagne</p>
-            </div>
-           
-            
+            </div>    
         </div>
     </div>
+    <div className='container4'>
+       <label className='date'>la date du jour</label>
+       <div className='container5'>
+     
+       <div className='box'>
+           <div className='labeldiv1'>
+           <label className='label1'>20</label>
+           </div>
+           <div className='boxDiv1'>
+               <div >
+               <label>Total soldes</label>
+               <p>$25,042</p>
+               </div>
+               <div>
+                   <label>snipper</label>
+               </div>
+           </div>
+           <label>last one</label>
+       </div>
+       <div className='box'>
+           <div className='labeldiv2'>
+           <label className='label1'>10</label>
+           </div>
+           <div className='boxDiv1'>
+               <div >
+               <label>Total soldes</label>
+               <p>$25,042</p>
+               </div>
+               <div>
+                   <label>snipper</label>
+               </div>
+           </div>
+           <label>last one</label>
+       </div>
+       <div className='box'>
+           <div className='labeldiv3'>
+           <label className='label1'>5</label>
+           </div>
+           <div className='boxDiv1'>
+               <div >
+               <label>Total soldes</label>
+               <p>$25,042</p>
+               </div>
+               <div>
+                   <label>snipper</label>
+               </div>
+           </div>
+           <label>last one</label>
+       </div>
+       </div>
+    </div>
+    </div> 
   )
 }
