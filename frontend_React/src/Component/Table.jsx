944d882da083b29e5d3a6e9e878fe39f7806e8a0
Chabrik Abdellatif <abdoo.chbrik@gmail.com>
2022-03-24 11:11:40 +0100

    update table

--- frontend_React/src/Component/Table.jsx
+++ frontend_React/src/Component/Table.jsx
@@ -31,12 +31,12 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Frozen yoghurt', 'Frozen yoghurt', 'oui', 24,),
+  createData('Ice cream sandwich','Frozen yoghurt', 'non',37, ),
+  createData('Eclair','Frozen yoghurt', 'non', 16.0,),
+  createData('Cupcake','Frozen yoghurt', 'oui', 4.3),
+  createData('Gingerbread','Frozen yoghurt', 'non',  3.9),
+  createData('Gingerbread','Frozen yoghurt', 'non',  3.9),
 ];
 
 const useStyles = makeStyles({
@@ -53,11 +53,10 @@ export default function CustomizedTables() {
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
           <TableRow>
-            <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-            <StyledTableCell align="right">Calories</StyledTableCell>
-            <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-            <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+            <StyledTableCell>Départ</StyledTableCell>
+            <StyledTableCell align="right">Arrivé</StyledTableCell>
+            <StyledTableCell align="right">retard</StyledTableCell>
+            <StyledTableCell align="right">temps de retard</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -69,7 +68,7 @@ export default function CustomizedTables() {
               <StyledTableCell align="right">{row.calories}</StyledTableCell>
               <StyledTableCell align="right">{row.fat}</StyledTableCell>
               <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+              
             </StyledTableRow>
           ))}
         </TableBody>
